cd93c8414250373f5db3adf8c0243d20e28dbe4e
iapposki <konark981999@gmail.com>
2023-05-02 15:33:38 +0530

    containarized the app

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const { signUp, login, forgotPassword, resetPassword, verifyUser } = require('./
 const { validateOtp, createOtp, resendOtp } = require('./controllers/otp.controller');
 const { createOrder } = require('./controllers/order.controller');
 const { authenticate } = require('./middlewares/auth');
+const { PrismaClient } = require('@prisma/client');
 const app = express();
 const port = 3000;
 
@@ -37,8 +38,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
 app.use(cors());
 
-app.get('/status', (req, res) => {
+app.get('/status', async (req, res) => {
     // res.status(200).json({msg:'GET request received'});
+    // const prisma = new PrismaClient()
+    // const record = await prisma.user.findMany()
+    // console.log(record)
+    // console.log('node server is running')
     res.send("Node server is running")
 })
 
